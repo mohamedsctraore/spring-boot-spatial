35742a186c5c5959e6edea11058b849e95e80142
Mohamed Traore <mohamed.s.c.traore@gmail.com>
2018-12-17 09:07:42 +0000

    Initialisation projet Spring

---
+++ src/main/java/com/mohamedsctraore/spatial/SpringBootSpatialApplication.java
@@ -0,0 +1,14 @@
+package com.mohamedsctraore.spatial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpatialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpatialApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/mohamedsctraore/spatial/SpringBootSpatialApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mohamedsctraore.spatial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSpatialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
